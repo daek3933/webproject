b63e4f8acce5893f51c88aba33fc500e5ee4a811
Zubair Afzal <piratedark002@gmail.com>
2020-08-29 02:24:03 +0500

    commit

--- database/migrations/2020_08_28_212224_update_product_table.php
+++ database/migrations/2020_08_28_212224_update_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBlogsCommentsTable extends Migration
+class UpdateProductTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,7 @@ class CreateBlogsCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('blogs_comments', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -26,6 +23,6 @@ class CreateBlogsCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('blogs_comments');
+        //
     }
 }
